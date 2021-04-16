44cd7f2e89be9ee5764ebabb229f5d8ab5425917
Denys Bishyr <73912929+Den4ikB@users.noreply.github.com>
2021-04-16 22:30:30 +0300

    Add files via upload

---
+++ Models/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ISTTPLab1
+{
+    public partial class Order
+    {
+        public Order()
+        {
+            OrderItems = new HashSet<OrderItem>();
+        }
+
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int StatusId { get; set; }
+        public DateTime Date { get; set; }
+
+        public virtual Status Status { get; set; }
+        public virtual User User { get; set; }
+        public virtual ICollection<OrderItem> OrderItems { get; set; }
+    }
+}
